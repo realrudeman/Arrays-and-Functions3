c6c1c8bd9101bb688a35c3178bee24cb7ee36a61
Ruden <stocky67@mail.ru>
2023-12-21 00:48:53 +0500

    done 3_3

---
+++ Program3_3.cs
@@ -0,0 +1,28 @@
+﻿int size = 10;
+
+double[] arr = new double[size];
+
+Random rand = new Random();
+for (int i = 0; i < size; i++)
+{
+    arr[i] = rand.NextDouble() * 100;
+}
+
+Console.WriteLine("Массив:");
+foreach(double num in arr)
+{
+    Console.Write(num + " ");
+}
+Console.WriteLine();
+
+double max = arr[0];
+double min = arr[0];
+
+for (int i = 0; i < arr.Length; i++)
+{
+    if (arr[i] < min)
+        min = arr[i];
+    else if (arr[i] > max)
+        max = arr[i];
+}
+Console.WriteLine("Разница между максимальным и минимальным значением массива: " + (max - min));
